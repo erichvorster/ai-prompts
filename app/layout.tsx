9f5f533ade57512db8a5407b96188a207849b849
erichvorster <erichv@infoslips.com>
2023-05-12 16:29:42 +0200

    implemented base template and started auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,29 @@
-import './globals.css'
+import "../styles/globals.css";
+import Nav from "@/components/Nav";
+import Provider from "@/components/Provider";
 
 
-
-
+export const metadata = {
+  title: "Prompti",
+  description: "Discover & Share Ai Prompts",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav/>
+        {children}
+        </main>
+      </body>
     </html>
-  )
+  );
 }
